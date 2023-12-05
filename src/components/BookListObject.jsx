bfcb83a46ffc271c664e5048abc75213f6c36c7d
Mel <meljohnpualon@gmail.com>
2023-12-05 17:40:50 +0800

    added iteration books in props

--- src/components/BookListObject.jsx
+++ src/components/BookListObject.jsx
@@ -17,17 +17,54 @@ const books = [
   }
 ];
 
-const names = ['Mel',  'John', 'doe']
-const newsNames = names.map( name => {
-  return <h1>{name}</h1>
-})
+// const names = ['Mel',  'John', 'doe']
+// const newsNames = names.map( name => {
+//   return <h1>{name}</h1>
+// })
+
+// ITERATING BOOKLIST ON REGULAR STEP
+// const BookList = () => {
+//   return (
+//     <div className="container">
+//       {/* <section className="book-list">
+//         {names}
+//       </section> */}
+//       <section className="book-list">
+//         {books.map(item => {
+
+//           return (
+//             <div className="book">
+//               <img src={item.img} alt={item.title} />
+//               <h2>{item.title}</h2>
+//               <p>{item.author}</p>
+//             </div>
+//           )
+//         })}
+//       </section>
+//     </div>
+//   )
+// }
+
+// ITERATING BOOKLIST ON PROPS
 const BookList = () => {
   return (
-    <div className="container">
-      {/* <section className="book-list">
-        {names}
-      </section> */}
-      <section className="book-list">{newsNames}</section>
-    </div>
+    <section className="book-list"> 
+      {books.map(item => {
+        const { img, title, author } = item;
+        return <Book img={img} title={title} author={author}/>
+      })}
+    </section>
+  )
+}
+
+const Book = (props) => {
+  const { img, title, author } = props;
+
+  return(
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <p>{author}</p>
+    </article>
   )
 }
