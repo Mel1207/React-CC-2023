972b78009fbf7ddb2081dcac7a84268a941b45eb
Mel <meljohnpualon@gmail.com>
2023-12-08 05:05:34 +0800

    added local images

--- src/components/ES6Modules/books.js
+++ src/components/ES6Modules/books.js
@@ -1,14 +1,24 @@
+import img1 from '../../images/bookReact.png'
+import img2 from '../../images/book1.jpg'
+import img3 from '../../images/book2.jpg'
+
 export const books = [
   {
     author: 'Mel Pualon',
     title: 'Learning React 2023',
-    img: './images/bookReact.png',
+    img: img1,
     id: 1
   },
   {
     author: 'Mel Pualon',
     title: 'Learning Frontend 2023',
-    img: './images/book2.jpg',
+    img: img2,
     id: 2
+  },
+  {
+    author: 'Mel Pualon',
+    title: 'Test book 3',
+    img: img3,
+    id: 3
   }
 ];
